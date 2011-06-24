5aba8554e54f5aabcae836baa238e345c4514cdb
Karl Bowden <karlbowden@gmail.com>
2011-06-24 19:58:00 +1000

    Added sass_compass and sass_less mediagenerator plugin modules

--- html5boilerplate/media.py
+++ html5boilerplate/media.py
@@ -7,20 +7,28 @@
 
 # Or copy the required parts from below to your MEDIA_BUNDLES in settings.py
 
+_js_head_bundle = (
+    'js/libs/modernizr-2.0.min.js',
+    'js/libs/respond.js',
+)
+
+_js_main_bundle = (
+    {'filter': 'mediagenerator.filters.media_url.MediaURL'},
+    'js/libs/jquery-1.6.1.js',
+    'js/plugins.js',
+    'js/script.js',
+)
+
 HTML5_MEDIA_BUNDLES = (
     ('style.css',
         'css/style.css',
     ),
 
-    ('head.js',
-        'js/libs/modernizr-2.0.min.js',
-        'js/libs/respond.js',
-    ),
-
-    ('main.js',
-        {'filter': 'mediagenerator.filters.media_url.MediaURL'},
-        'js/libs/jquery-1.6.1.js',
-        'js/plugins.js',
-        'js/script.js',
-    ),
+    ('head-ie.js',)
+        + _js_head_bundle,
+    ('main-ie.js',)
+        + _js_main_bundle,
+    ('main.js',)
+        + _js_head_bundle
+        + _js_main_bundle,
 )
